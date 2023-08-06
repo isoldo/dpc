4a3d6b1eef407688b9f9a700f442352418272b63
isoldo <ivan@calimero.network>
2023-08-06 21:36:17 +0100

    chore(app): extract calculator logic and add tests (#13)

--- src/delivery/index.ts
+++ src/delivery/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { createDelivery, createUser, fetchFixedPrices, fetchUserByMail, fetchVariablePrices } from "../db/index.js";
 import { errorFactory } from "../utils/errorFactory.js";
+import { calculateDeliveryPrice } from "./calculator.js";
 
 export interface DeliveryParameters {
   packageCount: number;
@@ -23,33 +24,10 @@ export async function deliveryHandler(req: express.Request, res: express.Respons
       return errorFactory(res, 500, "Error creating a user in DB");
     }
   }
-  const fixedPrices = await fetchFixedPrices();
-  if (!fixedPrices) {
-    return errorFactory(res, 500, "Fixed prices not defined");
-  }
-  const variablePrices = await fetchVariablePrices();
-  if (!variablePrices) {
-    return errorFactory(res, 500, "Variable prices not defined");
-  }
 
-  const baseCost = fixedPrices.base;
-  const additionalPackageCost = (packageCount - 1) * fixedPrices.additionalPackage;
-  let distanceCost = 0;
-
-  for (const vp of variablePrices) {
-    console.debug({ vp });
-    if (vp.start < distance) {
-      console.debug(`Distance greater than start of interval (${vp.start})`);
-      if (vp.end === -1 || vp.end > distance) {
-        console.debug(`Distance fits in the interval [${vp.start}, ${vp.end}]`);
-        console.debug(`Cost += ${(distance-vp.start) * vp.cost}`);
-        distanceCost += (distance - vp.start) * vp.cost;
-      } else {
-        console.debug(`Distance exceeds the interval [${vp.start}, ${vp.end}]`);
-        console.debug(`Cost += ${(vp.end-vp.start)*vp.cost}`);
-        distanceCost += (vp.end - vp.start) * vp.cost;
-      }
-    }
+  const calculatedPrice = await calculateDeliveryPrice(res, distance, packageCount);
+  if (!calculatedPrice) {
+    return;
   }
 
   const data: DeliveryParameters = {
@@ -57,10 +35,10 @@ export async function deliveryHandler(req: express.Request, res: express.Respons
     packageCount,
     date: new Date(date),
     userId: user.id,
-    cost: baseCost + additionalPackageCost + distanceCost,
-    baseCost,
-    additionalPackageCost,
-    distanceCost
+    cost: calculatedPrice.price,
+    baseCost: calculatedPrice.base,
+    additionalPackageCost: calculatedPrice.additionalPackages,
+    distanceCost: calculatedPrice.distance
   };
 
   console.debug({ deliveryParameters: data });
