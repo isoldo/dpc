487cbefee9ebb6bb352f01b2ca753d18dc8b2b22
isoldo <ivan@calimero.network>
2023-08-06 22:50:01 +0100

    feat(app): add admin route auth (#14)
    * install jsonwebtoken
    
    * feat(app): implement jwt auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,25 @@
 import express from "express";
+import 'dotenv/config';
+import jwt from "jsonwebtoken";
+
 
 export async function isAuthorized(req: express.Request): Promise<boolean> {
-  // TODO: implement JWT authentication
-  console.warn(">>> WARNING: isAuthorized is not yet implemented");
-  return true;
+  const token = req.headers.authorization;
+  const secretKey = process.env.SECRET_KEY as string;
+
+  if (!token) {
+    return false;
+  }
+
+  try {
+    const payload = jwt.verify(token, secretKey) as jwt.JwtPayload;
+
+    console.debug({ payload });
+
+    return payload.isAdmin;
+  } catch (e) {
+    const err = e as Error;
+    console.error(`>>>>>> ERROR: ${err.message}`);
+    return false;
+  }
 }
