ccb7d59549204ae2c876f106667d6056dad0aeab
Ivan Soldo <ivan@calimero.network>
2023-08-08 15:05:42 +0200

    define function for shorter calls for login and health

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -5,8 +5,8 @@ import { createHash } from "crypto";
 import jwt from "jsonwebtoken";
 
 let server;
-let email: string;
-let pw = "password";
+const email = "dummy@admin.mail";
+const pw = "password";
 let token: string;
 const wrongToken = jwt.sign({ isAdmin: true }, "wrongKey" );
 
@@ -20,7 +20,6 @@ const urls = {
 beforeAll(async () => {
   server = listener;
   const prismaClient = new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL} } });
-  email = "dummy@admin.mail";
   const passwordHash = createHash("sha256").update(pw, "ascii").digest().toString("hex");
   const admin = await prismaClient.admin.create({ data: { email, passwordHash }});
   console.log({ url: process.env.DATABASE_URL, admin });
@@ -32,16 +31,18 @@ afterAll(async () => {
 
 describe("Check the health API", () =>
   {
+    const get = async () => request(server).get(urls.health);
+    const post = async () => request(server).post(urls.health).send({});
     it('should respond with status 200 and body { status: "ALIVE" } to a GET request',
       async () => {
-        const response = await request(server).get(urls.health);
+        const response = await get();
         expect(response.status).toBe(200);
         expect(response.body).toEqual({ status: "ALIVE" });
       }
     );
     it('should respond with status 200 and body { status: "ALIVE" } to a POST request',
       async () => {
-        const response = await request(server).post(urls.health).send({});
+        const response = await post();
         expect(response.status).toBe(200);
         expect(response.body).toEqual({ status: "ALIVE" });
       }
@@ -51,33 +52,34 @@ describe("Check the health API", () =>
 
 describe("Check admin login API", () =>
   {
+    const login = async (params: {un?: string, pw?: string}) => request(server).post(urls.login).send({ ...params });
     it("should return 400 for missing email",
       async () => {
-        const response = await request(server).post(urls.login).send({ pw: "dont-care" });
+        const response = await login({ pw: "dont-care" });;
         expect(response.status).toBe(400);
       }
     );
     it("should return 400 for missing password",
       async () => {
-        const response = await request(server).post(urls.login).send({ un: "dont-care" });
+        const response = await login({ un: "dont-care" });
         expect(response.status).toBe(400);
       }
     );
     it("should return 401 for a non-existing admin",
       async () => {
-        const response = await request(server).post(urls.login).send({ un: "dont-exist@corp.com", pw});
+        const response = await login({ un: "dont-exist@corp.com", pw});
         expect(response.status).toBe(401);
       }
     );
     it("should return 401 for a wrong password",
       async () => {
-        const response = await request(server).post(urls.login).send({ un: email, pw: "wrongpass"});
+        const response = await login({ un: email, pw: "wrongpass"});
         expect(response.status).toBe(401);
       }
     );
     it("should return a verifiable JWT for an existing admin",
       async () => {
-        const response = await request(server).post(urls.login).send({ un: email, pw});
+        const response = await login({ un: email, pw});
         token = response.body.token;
         const verifiedToken = jwt.verify(token, (process.env.SECRET_KEY as string)) as jwt.JwtPayload;
         const isAdmin = verifiedToken.isAdmin;
