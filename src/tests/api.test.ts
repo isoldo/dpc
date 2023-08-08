0753825fd8ebf3dce58b6895e69e95e36c91e664
Ivan Soldo <ivan@calimero.network>
2023-08-08 14:02:50 +0200

    extract urls to an object

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -10,6 +10,13 @@ let pw = "password";
 let token: string;
 const wrongToken = jwt.sign({ isAdmin: true }, "wrongKey" );
 
+const urls = {
+  health: "/api/health",
+  login: "/api/admin/login",
+  fixed: "/api/admin/prices/fixed",
+  variable: "/api/admin/prices/variable"
+};
+
 beforeAll(async () => {
   server = listener;
   const prismaClient = new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL} } });
@@ -27,14 +34,14 @@ describe("Check the health API", () =>
   {
     it('should respond with status 200 and body { status: "ALIVE" } to a GET request',
       async () => {
-        const response = await request(server).get('/api/health');
+        const response = await request(server).get(urls.health);
         expect(response.status).toBe(200);
         expect(response.body).toEqual({ status: "ALIVE" });
       }
     );
     it('should respond with status 200 and body { status: "ALIVE" } to a POST request',
       async () => {
-        const response = await request(server).post('/api/health').send({});
+        const response = await request(server).post(urls.health).send({});
         expect(response.status).toBe(200);
         expect(response.body).toEqual({ status: "ALIVE" });
       }
@@ -46,31 +53,31 @@ describe("Check admin login API", () =>
   {
     it("should return 400 for missing email",
       async () => {
-        const response = await request(server).post("/api/admin/login").send({ pw: "dont-care" });
+        const response = await request(server).post(urls.login).send({ pw: "dont-care" });
         expect(response.status).toBe(400);
       }
     );
     it("should return 400 for missing password",
       async () => {
-        const response = await request(server).post("/api/admin/login").send({ un: "dont-care" });
+        const response = await request(server).post(urls.login).send({ un: "dont-care" });
         expect(response.status).toBe(400);
       }
     );
     it("should return 401 for a non-existing admin",
       async () => {
-        const response = await request(server).post("/api/admin/login").send({ un: "dont-exist@corp.com", pw});
+        const response = await request(server).post(urls.login).send({ un: "dont-exist@corp.com", pw});
         expect(response.status).toBe(401);
       }
     );
     it("should return 401 for a wrong password",
       async () => {
-        const response = await request(server).post("/api/admin/login").send({ un: email, pw: "wrongpass"});
+        const response = await request(server).post(urls.login).send({ un: email, pw: "wrongpass"});
         expect(response.status).toBe(401);
       }
     );
     it("should return a verifiable JWT for an existing admin",
       async () => {
-        const response = await request(server).post("/api/admin/login").send({ un: email, pw});
+        const response = await request(server).post(urls.login).send({ un: email, pw});
         token = response.body.token;
         const verifiedToken = jwt.verify(token, (process.env.SECRET_KEY as string)) as jwt.JwtPayload;
         const isAdmin = verifiedToken.isAdmin;
@@ -85,49 +92,49 @@ describe("Check admin API authorization", () =>
   {
     it("should return 401 for GET fixed prices with no token",
       async () => {
-        const response = await request(server).get("/api/admin/prices/fixed");
+        const response = await request(server).get(urls.fixed);
         expect(response.status).toBe(401);
       }
     );
     it("should return 401 for GET fixed prices with a wrong token",
       async () => {
-        const response = await request(server).get("/api/admin/prices/fixed").set("authorization", wrongToken);
+        const response = await request(server).get(urls.fixed).set("authorization", wrongToken);
         expect(response.status).toBe(401);
       }
     );
     it("should return 401 for PUT fixed prices with no token",
       async () => {
-        const response = await request(server).put("/api/admin/prices/fixed").send({});
+        const response = await request(server).put(urls.fixed).send({});
         expect(response.status).toBe(401);
       }
     );
     it("should return 401 for PUT fixed prices with a wrong token",
       async () => {
-        const response = await request(server).put("/api/admin/prices/fixed").set("authorization", wrongToken).send({});
+        const response = await request(server).put(urls.fixed).set("authorization", wrongToken).send({});
         expect(response.status).toBe(401);
       }
     );
     it("should return 401 for GET variable prices with no token",
       async () => {
-        const response = await request(server).get("/api/admin/prices/variable");
+        const response = await request(server).get(urls.variable);
         expect(response.status).toBe(401);
       }
     );
     it("should return 401 for GET variable prices with a wrong token",
       async () => {
-        const response = await request(server).get("/api/admin/prices/variable").set("variable", wrongToken);
+        const response = await request(server).get(urls.variable).set("variable", wrongToken);
         expect(response.status).toBe(401);
       }
     );
     it("should return 401 for PUT variable prices with no token",
       async () => {
-        const response = await request(server).put("/api/admin/prices/variable").send({});
+        const response = await request(server).put(urls.variable).send({});
         expect(response.status).toBe(401);
       }
     );
     it("should return 401 for PUT variable prices with a wrong token",
       async () => {
-        const response = await request(server).put("/api/admin/prices/variable").set("variable", wrongToken).send({});
+        const response = await request(server).put(urls.variable).set("variable", wrongToken).send({});
         expect(response.status).toBe(401);
       }
     );
@@ -146,47 +153,47 @@ describe("Check the fixed prices API", () =>
     };
     it("should return 404 for GET fixed prices when DB is empty",
       async () => {
-        const response = await request(server).get("/api/admin/prices/fixed").set("authorization", token);
+        const response = await request(server).get(urls.fixed).set("authorization", token);
         expect(response.status).toBe(404);
       }
     );
     it("should return 200 for PUT fixed prices when DB is empty",
       async () => {
-        const response = await request(server).put("/api/admin/prices/fixed").set("authorization", token).send(fixedPrices1);
+        const response = await request(server).put(urls.fixed).set("authorization", token).send(fixedPrices1);
         expect(response.status).toBe(200);
         expect(response.body.data).toStrictEqual({ ...fixedPrices1, active: true, id: 1});
       }
     );
     it("should return 200 and the correct values for GET fixed prices",
       async () => {
-        const response = await request(server).get("/api/admin/prices/fixed").set("authorization", token);
+        const response = await request(server).get(urls.fixed).set("authorization", token);
         expect(response.status).toBe(200);
         expect(response.body.data).toStrictEqual({ ...fixedPrices1, active: true, id: 1});
       }
     );
     it("should return 200 for PUT fixed prices when DB is not empty and id should be 2",
       async () => {
-        const response = await request(server).put("/api/admin/prices/fixed").set("authorization", token).send(fixedPrices2);
+        const response = await request(server).put(urls.fixed).set("authorization", token).send(fixedPrices2);
         expect(response.status).toBe(200);
         expect(response.body.data).toStrictEqual({ ...fixedPrices2, active: true, id: 2});
       }
     );
     it("should return 200 and the correct updated values for GET fixed prices",
       async () => {
-        const response = await request(server).get("/api/admin/prices/fixed").set("authorization", token);
+        const response = await request(server).get(urls.fixed).set("authorization", token);
         expect(response.status).toBe(200);
         expect(response.body.data).toStrictEqual({ ...fixedPrices2, active: true, id: 2});
       }
     );
     it("should return 400 for PUT fixed prices with missing additionalPackage cost param",
       async () => {
-        const response = await request(server).put("/api/admin/prices/fixed").set("authorization", token).send({ base: 4 });
+        const response = await request(server).put(urls.fixed).set("authorization", token).send({ base: 4 });
         expect(response.status).toBe(400);
       }
     );
     it("should return 400 for PUT fixed prices with missing base price param",
       async () => {
-        const response = await request(server).put("/api/admin/prices/fixed").set("authorization", token).send({ additionalPackage: 4 });
+        const response = await request(server).put(urls.fixed).set("authorization", token).send({ additionalPackage: 4 });
         expect(response.status).toBe(400);
       }
     );
