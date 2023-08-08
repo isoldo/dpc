35768f633581dbe933841cc2a1a6c55111d19662
Ivan Soldo <ivan@calimero.network>
2023-08-08 19:24:25 +0200

    test delivery endpoint with missing params

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -308,7 +308,7 @@ describe("Check the request delivery API", () =>
       date?: number;
       name?: string;
       lastName?: string;
-    }
+    };
     const post = async (params: DeliveryParams) => request(server).post(urls.delivery).send(params);
     const validDeliveryRequest1 = {
       distance: 3,
@@ -318,7 +318,15 @@ describe("Check the request delivery API", () =>
       date: Date.now(),
       name: "Name",
       lastName: "Last Name"
-    }
+    };
+    const invalidDeliveryRequest1 = {
+      distance: 3,
+      packageCount: 1,
+      email: "dummy@customer.com",
+      phone: "+385951234567",
+      name: "Name",
+      lastName: "Last Name"
+    };
     it("should return 405 for a non POST delivery request",
       async () => {
         const response = await request(server).get(urls.delivery);
@@ -329,7 +337,6 @@ describe("Check the request delivery API", () =>
     it("should return 200 for POST delivery request, calculate the correct price and not send mail from the test suite",
       async () => {
         const response = await post(validDeliveryRequest1);
-        console.log(response.body)
         expect(response.status).toBe(200);
         expect(response.body.data.cost).toBe(3 + 3*2);
         expect(response.body.mailStatus).toEqual("not sent");
@@ -338,11 +345,17 @@ describe("Check the request delivery API", () =>
     it("should return 200 for POST delivery request, calculate the correct price and not send mail from the test suite",
       async () => {
         const response = await post({ ...validDeliveryRequest1, packageCount: 7, distance: 13 });
-        console.log(response.body)
         expect(response.status).toBe(200);
         expect(response.body.data.cost).toBe(3 + (7-1)*0.75 + 4*2 + (11-4)*0.9 + (13-11)*0.7);
         expect(response.body.mailStatus).toEqual("not sent");
       }
     );
+    it("should return 400 for POST delivery request with missing params",
+      async () => {
+        const response = await post(invalidDeliveryRequest1);
+        expect(response.status).toBe(400);
+      }
+    );
+    // TODO test 500 responses
   }
 );
